f8b4b86a7c5f4abcefb0e046802027e5f51952c0
aubreyb11 <aubreyb132@gmail.com>
2025-09-25 18:23:36 -0500

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -36,6 +36,10 @@ android {
             // TODO: Add your own signing config for the release build.
             // Signing with the debug keys for now, so `flutter run --release` works.
             signingConfig = signingConfigs.getByName("debug")
+            getByName("release") {
+                isMinifyEnabled = false
+                isShrinkResources = false
+            }
         }
     }
 }
